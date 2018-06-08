1bb32cfba55266727144e72c865139f4422bf7a4
Michal Szorád <miso.szorad@gmail.com>
2018-06-08 11:18:36 +0200

    Refactor reducers index

--- src/index.js
+++ src/index.js
@@ -9,13 +9,35 @@ import configureStore from './store/configureStore'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
-import * as reducers from './reducers'
 import { combineReducers } from 'redux'
+
+// reducers
+import { createPageReducer } from './reducers'
+import {
+  FETCH_POKEMONS_REQUEST,
+  FETCH_POKEMONS_SUCCESS,
+  FILTER_POKEMONS
+} from './constants/Page'
 
 const middlewares = [
   thunkMiddleware,
   process.env.NODE_ENV === 'development' && createLogger()
 ].filter(Boolean)
+
+const reducers = {
+  page: createPageReducer(
+    {
+      isFetched: false,
+      pokemons: [],
+      displayedPokemons: []
+    },
+    {
+      REQUEST: FETCH_POKEMONS_REQUEST,
+      SUCCESS: FETCH_POKEMONS_SUCCESS,
+      FILTER: FILTER_POKEMONS
+    }
+  )
+}
 
 const store = configureStore(
   createStore,
